2ddf6a98828f914b38909713d6c64aa9db15aba6
Le Tuan <letuanhf@gmail.com>
2025-09-05 10:46:26 +0700

    m

--- sweep_scripts/other_test/fig56_com1.sh
+++ sweep_scripts/other_test/fig56_com1.sh
@@ -1,3 +1,2 @@
 python system/main.py --cfp ./hparams/imagenet1k/FedAvg.json --seval --wandb True
 python system/main.py --cfp ./hparams/imagenet1k/FedDBE.json --seval --wandb True
-python system/main.py --cfp ./hparams/imagenet1k/FedSTGM.json --seval --wandb True
--- sweep_scripts/other_test/fig56_com4.sh
+++
@@ -1,3 +0,0 @@
-python system/main.py --cfp ./hparams/cifar100/FedAvg_cifar100.json --seval --wandb True --cpt 20 --nt 50 --note 20classes
-python system/main.py --cfp ./hparams/cifar100/FedDBE_cifar100.json --seval --wandb True --cpt 20 --nt 50 --note 20classes
-python system/main.py --cfp ./hparams/cifar100/FedSTGM_cifar100.json --seval --wandb True --cpt 20 --nt 50 --note 20classes
