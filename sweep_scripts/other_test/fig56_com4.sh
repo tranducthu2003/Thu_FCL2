98f28336c4d472e90f13f0f29244a603da5673f7
Le Tuan <letuanhf@gmail.com>
2025-09-05 13:11:04 +0900

    m

---
+++ sweep_scripts/other_test/fig56_com4.sh
@@ -0,0 +1,4 @@
+python system/main.py --cfp ./hparams/cifar100/FedALA_cifar100.json --seval --wandb True
+python system/main.py --cfp ./hparams/cifar100/FedALA_cifar100.json --seval --wandb True --cpt 20 --note 20classes
+python system/main.py --cfp ./hparams/imagenet1k/FedALA.json --seval --wandb True
+python3 system/main.py --cfp ./hparams/imagenet1k/FedDBE.json --seval --wandb True --cpt 20 --note 20classes
